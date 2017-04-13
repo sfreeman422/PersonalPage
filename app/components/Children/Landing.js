024e7f75f355a68b2b62ce827466e7826026a418
Steven Freeman <sfreeman422@gmail.com>
2017-04-13 13:06:21 -0400

    Removed all old CSS and begun restructure of application using React components

---
+++ app/components/Children/Landing.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default class Landing extends React.Component{
+	constructor(){
+		super();
+	}
+	render(){
+		return (
+			<div id="landing">
+				<h1>Steve Freeman</h1>
+				<h2>Full Stack Web Developer. Systems Administrator. Entrepreneur.</h2> 
+			</div>
+		)
+	}
+}
